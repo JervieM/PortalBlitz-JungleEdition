1f047273ca44513a7e2023ee23b2d8144152a57c
Jervie Manabat <jervie.manabat@student.ateneo.edu>
2025-01-14 16:06:38 +0800

    Update LoadSave.java

--- LoadSave.java
+++ LoadSave.java
@@ -2,21 +2,7 @@
 LoadSave class contains static methods for loading and saving images in a game. It has 
 methods for getting sprite atlases, loading levels, and getting individual images from files. 
 The class also contains a list of file names for different game assets.
-
-@author Jervie S. Manabat (223961), Giuliana Patricia Gabriele L. Bautista (220811)
-@version May 15,2023
 **/
-/*
-I have not discussed the Java language code in my program
-with anyone other than my instructor or the teaching assistants
-assigned to this course.
-I have not used Java language code obtained from another student,
-or any other unauthorized source, either modified or unmodified.
-If any Java language code or documentation used in my program
-was obtained from another source, such as a textbook or website,
-that has been clearly noted with a proper citation in the comments
-of my program.
-*/
 
 import java.awt.image.BufferedImage;
 import java.io.File;
