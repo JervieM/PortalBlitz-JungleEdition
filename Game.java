4526df4f6a93762cce194bedb9452685406b602b
Jervie Manabat <jervie.manabat@student.ateneo.edu>
2025-01-14 16:02:37 +0800

    Update Game.java

--- Game.java
+++ Game.java
@@ -3,21 +3,7 @@ Game class implements the Runnable interface, which allows the class to be execu
 The class contains methods for updating and rendering the game, as well as initializing classes for 
 the game's menu, gameplay, and game options. It also includes a game loop that continually updates 
 and renders the game, while tracking the frames per second (FPS) and updates per second (UPS).
-
-@author Jervie S. Manabat (223961), Giuliana Patricia Gabriele L. Bautista (220811)
-@version May 15,2023
 **/
-/*
-I have not discussed the Java language code in my program
-with anyone other than my instructor or the teaching assistants
-assigned to this course.
-I have not used Java language code obtained from another student,
-or any other unauthorized source, either modified or unmodified.
-If any Java language code or documentation used in my program
-was obtained from another source, such as a textbook or website,
-that has been clearly noted with a proper citation in the comments
-of my program.
-*/
 
 import java.awt.Graphics;
 
