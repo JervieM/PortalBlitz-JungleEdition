b5c38012016739c24bcbd3743fd038b9dc9fa90e
Jervie Manabat <jervie.manabat@student.ateneo.edu>
2025-01-14 16:09:52 +0800

    Update Shark.java

--- Shark.java
+++ Shark.java
@@ -1,21 +1,7 @@
 /**
 Shark class extends the Enemy class and defines the behavior and properties of a shark enemy in a game. 
 It has methods for updating the enemy's position and behavior based on the game state and player actions.
-
-@author Jervie S. Manabat (223961), Giuliana Patricia Gabriele L. Bautista (220811)
-@version May 15,2023
 **/
-/*
-I have not discussed the Java language code in my program
-with anyone other than my instructor or the teaching assistants
-assigned to this course.
-I have not used Java language code obtained from another student,
-or any other unauthorized source, either modified or unmodified.
-If any Java language code or documentation used in my program
-was obtained from another source, such as a textbook or website,
-that has been clearly noted with a proper citation in the comments
-of my program.
-*/
 
 public class Shark extends Enemy {
 
