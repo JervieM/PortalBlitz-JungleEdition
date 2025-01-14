5e54740d56d690e505458b9762ac6d9764570a53
Jervie Manabat <jervie.manabat@student.ateneo.edu>
2025-01-14 16:08:30 +0800

    Update ObjectManager.java

--- ObjectManager.java
+++ ObjectManager.java
@@ -1,21 +1,7 @@
 /**
 This is a class called ObjectManager that manages the objects in the game such as potions, containers, and 
 projectiles. It contains methods for checking collisions and updating object states.
-
-@author Jervie S. Manabat (223961), Giuliana Patricia Gabriele L. Bautista (220811)
-@version May 15,2023
 **/
-/*
-I have not discussed the Java language code in my program
-with anyone other than my instructor or the teaching assistants
-assigned to this course.
-I have not used Java language code obtained from another student,
-or any other unauthorized source, either modified or unmodified.
-If any Java language code or documentation used in my program
-was obtained from another source, such as a textbook or website,
-that has been clearly noted with a proper citation in the comments
-of my program.
-*/
 
 import java.awt.Graphics;
 import java.awt.geom.Rectangle2D;
