85b3ab42a7f1ea10c8a3072ae72471be54bcf13f
Jervie Manabat <jervie.manabat@student.ateneo.edu>
2025-01-14 16:05:03 +0800

    Update GameStarter.java

--- GameStarter.java
+++ GameStarter.java
@@ -2,21 +2,7 @@
 The GameStarter class is a main method that creates a new GameFrame instance with the dimensions 
 specified in the Game class, then calls its connectToServer and setUpGUI methods. This class is used 
 to start the game by initializing the graphical user interface and connecting to the game server.
-
-@author Jervie S. Manabat (223961), Giuliana Patricia Gabriele L. Bautista (220811)
-@version May 15,2023
 **/
-/*
-I have not discussed the Java language code in my program
-with anyone other than my instructor or the teaching assistants
-assigned to this course.
-I have not used Java language code obtained from another student,
-or any other unauthorized source, either modified or unmodified.
-If any Java language code or documentation used in my program
-was obtained from another source, such as a textbook or website,
-that has been clearly noted with a proper citation in the comments
-of my program.
-*/
 
 import java.util.*;
 public class GameStarter{
