a01def63f7079cbf636d2486bc0ee00f87d351c3
Jervie Manabat <jervie.manabat@student.ateneo.edu>
2025-01-14 16:08:56 +0800

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,21 +2,7 @@
 Player class extends from another class called "Entity". It contains various properties and methods related to 
 the behavior and appearance of a player character in a 2D platformer game, such as animations, movement, attack, 
 health and power bars, and collision detection with the environment.
-
-@author Jervie S. Manabat (223961), Giuliana Patricia Gabriele L. Bautista (220811)
-@version May 15,2023
 **/
-/*
-I have not discussed the Java language code in my program
-with anyone other than my instructor or the teaching assistants
-assigned to this course.
-I have not used Java language code obtained from another student,
-or any other unauthorized source, either modified or unmodified.
-If any Java language code or documentation used in my program
-was obtained from another source, such as a textbook or website,
-that has been clearly noted with a proper citation in the comments
-of my program.
-*/
 
 import java.awt.Color;
 import java.awt.Graphics;
