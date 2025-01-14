ae57f5fc15b9dfa44618c06ba75bcc8e347096d6
Jervie Manabat <jervie.manabat@student.ateneo.edu>
2025-01-14 16:07:59 +0800

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -2,21 +2,7 @@
 This is a class called Menu, which extends another class called State and implements an interface called Statemethods. 
 It contains several methods that handle the loading, updating, and drawing of a menu screen, including button 
 objects that are interacted with using mouse events.
-
-@author Jervie S. Manabat (223961), Giuliana Patricia Gabriele L. Bautista (220811)
-@version May 15,2023
 **/
-/*
-I have not discussed the Java language code in my program
-with anyone other than my instructor or the teaching assistants
-assigned to this course.
-I have not used Java language code obtained from another student,
-or any other unauthorized source, either modified or unmodified.
-If any Java language code or documentation used in my program
-was obtained from another source, such as a textbook or website,
-that has been clearly noted with a proper citation in the comments
-of my program.
-*/
 
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
