f174e9160d67c9f4ce8075b3ba0e4cfd4ef5fd70
Jervie Manabat <jervie.manabat@student.ateneo.edu>
2025-01-14 16:08:11 +0800

    Update MenuButton.java

--- MenuButton.java
+++ MenuButton.java
@@ -1,21 +1,7 @@
 /**
 The MenuButton class creates a button on a menu screen in a game. It contains an image and c
 an be interacted with using mouse events to trigger a Gamestate change when clicked.
-
-@author Jervie S. Manabat (223961), Giuliana Patricia Gabriele L. Bautista (220811)
-@version May 15,2023
 **/
-/*
-I have not discussed the Java language code in my program
-with anyone other than my instructor or the teaching assistants
-assigned to this course.
-I have not used Java language code obtained from another student,
-or any other unauthorized source, either modified or unmodified.
-If any Java language code or documentation used in my program
-was obtained from another source, such as a textbook or website,
-that has been clearly noted with a proper citation in the comments
-of my program.
-*/
 
 import java.awt.Graphics;
 import java.awt.Rectangle;
